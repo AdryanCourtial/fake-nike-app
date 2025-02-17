eae5987d34afd770491bf81839e4b66ff51f549e
AdryanCourtial <adryan.courtial@ynov.com>
2025-02-17 22:15:20 +0100

    :construction:

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from '@shopify/restyle';
 import { Stack } from 'expo-router';
 import { theme } from 'theme';
+import HeaderMain from '~/components/Global/Header';
 
 export default function Layout() {
   return (
     <ThemeProvider theme={theme}>
+      <HeaderMain></HeaderMain>
       <Stack />
     </ThemeProvider>
   );
